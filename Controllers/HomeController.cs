fd8ad4e8f35055140373ffb2750b94a2d36140b8
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-07 13:22:47 +0100

    web app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SquareWeb.Models;
+
+namespace SquareWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstNumber, string secondNumber)
+        {
+            int numberOne = int.Parse(firstNumber);
+            int numberTwo = int.Parse(secondNumber);
+            int result1 = Convert.ToInt32(Math.Sqrt(numberOne));
+            int result2 = Convert.ToInt32(Math.Sqrt(numberTwo));
+            ViewBag.number1 = numberOne;
+            ViewBag.number2 = numberTwo;
+            ViewBag.Result = result1;
+            ViewBag.Result1 = result2;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
